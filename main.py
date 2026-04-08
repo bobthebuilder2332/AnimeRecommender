16f13d4a25ebdcad7962909392ac2b51ad622eaa
bobthebuilder2332 <mayuheng120+github@gmail.com>
2026-04-08 15:52:48 -0400

    Added more filter fields and tested them

--- main.py
+++ main.py
@@ -1,23 +1,35 @@
 import requests
 
 # This is the URL of the API endpoint for Jikan API
-url = "https://api.jikan.moe/v4/anime/1"
+url = "https://api.jikan.moe/v4/anime/48736"
 
 # This is how you make a GET request to the API endpoint using the requests library
 response = requests.get(url)
 
 if response.status_code == 200: # Status Code 200 means success
-    # This converts the JSON response into a Python dictionary and ignores all the irrelevant data
+    # This converts the JSON response into a Python dictionary and ignores all the irrelevant data (outside 'data' field)
     data = response.json()['data']
     
     # Seperate data into specific fields
     url = data.get('url')
     title = data.get('title')
     score = data.get('score')
+    genres = [g.get('name') for g in data.get('genres', [])]
+    themes = [t.get('name') for t in data.get('themes', [])]
+    demographics = [d.get('name') for d in data.get('demographics', [])]
     episodes = data.get('episodes')
     synopsis = data.get('synopsis')
-    
+
+    #
+
     # Print data
-    print (f"URL: {url}\nTitle: {title}\nScore: {score}/10\nEpisodes: {episodes}\nSynopsis: {synopsis}")
+    print (f"URL: {url}")
+    print (f"Title: {title}")
+    print (f"Score: {score}/10")
+    print (f"Genre: {genres}")
+    print (f"Theme: {themes}")
+    print (f"Demographics: {demographics}")
+    print (f"Episodes: {episodes}")
+    print (f"Synopsis: {synopsis}")
 else:
     print (f"Error: {response.status_code}")
